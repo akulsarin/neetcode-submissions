b251d3ebf125e6f48586e0a66acf7cf0dfd3a5ac
akulsarin <70284895+akulsarin@users.noreply.github.com>
2026-08-04 09:08:14 -0700

    Add: valid-tree - submission-1

---
+++ Data Structures & Algorithms/valid-tree/submission-1.py
@@ -0,0 +1,43 @@
+class UnionFind:
+    def __init__(self, n: int):
+        self.par = {}
+        self.rank = {}
+
+        for i in range(n):
+            self.par[i] = i
+            self.rank[i] = 0
+
+    def find(self, n: int) -> int:
+        p = n
+        while p != self.par[p]:
+            self.par[p] = self.par[self.par[p]]
+            p = self.par[p]
+        return p
+
+    def union(self, n1: int, n2: int) -> bool:
+        p1, p2 = self.find(n1), self.find(n2)
+        if p1 == p2:
+            return False
+
+        if self.rank[p1] > self.rank[p2]:
+            self.par[p2] = p1
+        elif self.rank[p1] < self.rank[p2]:
+            self.par[p1] = p2
+        else:
+            self.par[p2] = p1
+            self.rank[p1] += 1
+
+        return True 
+
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        uf = UnionFind(n)
+        connectedComponents = n
+        for n1, n2 in edges:
+            if not uf.union(n1, n2):
+                return False
+            connectedComponents -= 1
+        return connectedComponents == 1
+
+        
+        
