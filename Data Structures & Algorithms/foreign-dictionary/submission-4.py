5b5d1f5433ea820f36fecdeabc952f5949da944d
akulsarin <70284895+akulsarin@users.noreply.github.com>
2026-08-04 12:42:36 -0700

    Add: foreign-dictionary - submission-4

---
+++ Data Structures & Algorithms/foreign-dictionary/submission-4.py
@@ -0,0 +1,30 @@
+class Solution:
+    def foreignDictionary(self, words: List[str]) -> str:
+        chars = {c for word in words for c in word}
+        adj = defaultdict(list)
+        inDegree = defaultdict(int)
+        for i in range(len(words) - 1):
+            a, b = words[i], words[i + 1]
+            for aChar, bChar in zip(a, b):
+                if aChar != bChar:
+                    adj[aChar].append(bChar)
+                    inDegree[bChar] += 1
+                    break
+            else:
+                if len(a) > len(b):
+                    return ""
+        
+        queue = deque([char for char in chars if inDegree[char] == 0])
+        result = []
+        while queue:
+            char = queue.popleft()
+            result.append(char)
+            for nextChar in adj[char]:
+                inDegree[nextChar] -= 1
+                if inDegree[nextChar] == 0:
+                    queue.append(nextChar)
+
+        if len(result) != len(chars):
+            return ""
+
+        return "".join(result)
