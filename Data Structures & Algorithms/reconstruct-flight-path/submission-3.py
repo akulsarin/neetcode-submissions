f2cc1866b4632e7b0d33c63b757a7c75a167de97
akulsarin <70284895+akulsarin@users.noreply.github.com>
2026-08-04 14:21:03 -0700

    Add: reconstruct-flight-path - submission-3

---
+++ Data Structures & Algorithms/reconstruct-flight-path/submission-3.py
@@ -0,0 +1,42 @@
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        adj = defaultdict(list)
+        for ticketId, ticket in enumerate(tickets):
+            src, dst = ticket
+            adj[src].append((dst, ticketId))
+
+        for src in adj:
+            adj[src].sort(key=lambda e:e[0])
+
+        n = len(tickets)
+        def dfs(itinerary: List[str], ticketsUsed: set) -> bool:
+            if len(ticketsUsed) == n:
+                return True
+
+            src = itinerary[-1]
+            for dst, ticketId in adj[src]:
+                if ticketId in ticketsUsed:
+                    continue
+                
+                ticketsUsed.add(ticketId)
+                itinerary.append(dst)
+                if dfs(itinerary, ticketsUsed):
+                    return True
+                itinerary.pop()
+                ticketsUsed.remove(ticketId)
+
+            return False
+
+        itinerary = ["JFK"]
+        dfs(itinerary, set())
+        return itinerary
+
+                
+
+            
+
+
+
+
+
+        
