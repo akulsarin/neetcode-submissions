635b69fbfa0dd5df287e33e57fcbf3529a36246f
akulsarin <70284895+akulsarin@users.noreply.github.com>
2026-08-06 16:38:11 -0700

    Add: count-subsequences - submission-4

---
+++ Data Structures & Algorithms/count-subsequences/submission-4.py
@@ -0,0 +1,18 @@
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        M, N = len(s), len(t)
+        if N > M:
+            return 0
+
+        dp = [[0] * (N + 1) for _ in range(M + 1)]
+        for i in range(M + 1):
+            dp[i][0] = 1 
+        
+        for i in range(1, M + 1):
+            for j in range(1, N + 1):
+                dp[i][j] = dp[i - 1][j]
+                
+                if s[i - 1] == t[j - 1]:
+                    dp[i][j] += dp[i - 1][j - 1]
+        
+        return dp[M][N]
