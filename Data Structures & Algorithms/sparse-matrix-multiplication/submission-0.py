8839a81172ef867d650c5cd651d5a451fd8f70d4
akulsarin <70284895+akulsarin@users.noreply.github.com>
2026-08-07 08:50:14 -0700

    Add: sparse-matrix-multiplication - submission-0

---
+++ Data Structures & Algorithms/sparse-matrix-multiplication/submission-0.py
@@ -0,0 +1,12 @@
+class Solution:
+    def multiply(self, mat1: List[List[int]], mat2: List[List[int]]) -> List[List[int]]:
+        M, K, N = len(mat1), len(mat1[0]), len(mat2[0])
+        result = [[0] * N for _ in range(M)]
+
+        for i in range(M):
+            for j in range(N):
+                for k in range(K):
+                    result[i][j] += (mat1[i][k] * mat2[k][j])
+        
+        return result
+        
