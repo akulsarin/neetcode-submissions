144c8c8ad1344c77eb22e5d39da611eb699d196b
akulsarin <70284895+akulsarin@users.noreply.github.com>
2026-08-12 22:27:27 -0700

    Add: triangle - submission-0

---
+++ Data Structures & Algorithms/triangle/submission-0.py
@@ -0,0 +1,20 @@
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        N = len(triangle)
+
+        dp = [[0] * (N + 1) for _ in range(N + 1)]
+
+        for r in range(N - 1, -1, -1):
+            for c in range(r, -1, -1):
+                dp[r][c] = triangle[r][c]
+                dp[r][c] += min(dp[r + 1][c], dp[r + 1][c + 1])
+
+        return dp[0][0]
+
+        def dfs(r: int, c: int) -> int:
+            if r == N:
+                return 0
+
+            return triangle[r][c] + min(dfs(r + 1, c), dfs(r + 1, c + 1))
+
+        return dfs(0, 0)
