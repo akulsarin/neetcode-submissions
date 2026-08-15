fde604df764713221f5a055f5445bf0b63488298
akulsarin <70284895+akulsarin@users.noreply.github.com>
2026-08-15 14:15:53 -0700

    Add: insert-new-interval - submission-1

---
+++ Data Structures & Algorithms/insert-new-interval/submission-1.py
@@ -0,0 +1,18 @@
+import bisect
+
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        N = len(intervals)
+
+        startIdx = bisect.bisect_left(intervals, newInterval)
+        if startIdx > 0 and intervals[startIdx - 1][1] >= newInterval[0]:
+            startIdx -= 1
+            newInterval[0] = intervals[startIdx][0]
+            newInterval[1] = max(intervals[startIdx][1], newInterval[1])
+
+        endIdx = startIdx - 1
+        while endIdx + 1 < N and newInterval[1] >= intervals[endIdx + 1][0]:
+            endIdx += 1
+            newInterval[1] = max(intervals[endIdx][1], newInterval[1])
+
+        return intervals[:startIdx] + [newInterval] + intervals[endIdx + 1:]
