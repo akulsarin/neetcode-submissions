07ae26a754f5e012d53f306a7640de95311c430d
akulsarin <70284895+akulsarin@users.noreply.github.com>
2026-08-27 07:55:51 -0700

    Add: pacific-atlantic-water-flow - submission-3

---
+++ Data Structures & Algorithms/pacific-atlantic-water-flow/submission-3.py
@@ -0,0 +1,30 @@
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        rows, cols = len(heights), len(heights[0])
+        dirs = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+        def bfs(start_queue: List[Tuple[int, int]]) -> set[Tuple[int, int]]:
+            queue = deque(start_queue)
+            visit = set(start_queue)
+            while queue:
+                r, c = queue.popleft()
+                for dr, dc in dirs:
+                    r2, c2 = r + dr, c + dc
+                    if 0 <= r2 < rows and 0 <= c2 < cols and (r2, c2) not in visit and heights[r2][c2] >= heights[r][c]: 
+                        queue.append((r2, c2))
+                        visit.add((r2, c2))
+            return visit
+
+        atlantic_edges = []
+        pacific_edges = []
+        for r in range(rows):
+            for c in range(cols):
+                if r == 0 or c == 0:
+                    pacific_edges.append((r, c))
+                if r == rows - 1 or c == cols - 1:
+                    atlantic_edges.append((r, c))
+
+        atlantic_flows = bfs(atlantic_edges)
+        pacific_flows = bfs(pacific_edges)
+
+        return [list(coord) for coord in atlantic_flows & pacific_flows]
