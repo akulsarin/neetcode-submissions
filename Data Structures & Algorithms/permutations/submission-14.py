ad8036946d6e5fa6c373cdc1c928ba0ea6281236
akulsarin <70284895+akulsarin@users.noreply.github.com>
2026-08-31 18:28:33 -0700

    Add: permutations - submission-14

---
+++ Data Structures & Algorithms/permutations/submission-14.py
@@ -0,0 +1,20 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        N = len(nums)
+        permutations = []
+
+        def backtrack(curr: List[int], used: set[int]):
+            if len(curr) == N:
+                permutations.append(curr.copy())
+                return
+            
+            for num in nums:
+                if num not in used:
+                    curr.append(num)
+                    used.add(num)
+                    backtrack(curr, used)
+                    used.remove(num)
+                    curr.pop()
+        
+        backtrack([], set())
+        return permutations
